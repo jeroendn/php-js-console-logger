6d92aa0c66b248295950992a8003dc9f15fb8c53
jeroendn <51778824+jeroendn@users.noreply.github.com>
2021-10-05 12:10:32 +0200

    Updated generateJs and README

--- README.md
+++ README.md
@@ -1,7 +1,24 @@
-# php js console logger
+# php javascript console logger
 A simple way to create javascript console logs via a php function.
+
+This package has mostly been made as a joke and to get some experience setting up a package. Feel free to find any good use for it.
+
+![Alt Text](https://jeroendn.nl/media/ConsoleRickRoll.gif)
 
 ## Installation
 ```bash
 composer require jeroendn/php-js-console-logger
+```
+
+## How to use
+```php
+use jeroendn\phpJsConsoleLogger\phpJsConsoleLogger;
+
+$logger = new phpJsConsoleLogger();
+
+$logger = $logger->setLog('Example');
+// OR
+$logger = $logger->setLogs(['Example1', 'Example2', 'Example3']);
+
+echo $logger->generateJs(); // Print the generated JS in your HTML
 ```
--- src/phpJsConsoleLogger.php
+++ src/phpJsConsoleLogger.php
@@ -2,15 +2,18 @@
 
 namespace jeroendn\phpJsConsoleLogger;
 
+use DateTime;
+
 class phpJsConsoleLogger
 {
   private string $log = '';
   private array $logs = [];
-
-  public function __construct()
-  {
-
-  }
+//  private int $timeout = 0;
+//  private int $interval = 1000;
+//  private int $maxIterations = -1; // Set to -1 (any negative number) for infinite iterations
+//  private DateTime|null $dateTimeStart = null;
+//  private DateTime|null $dateTimeEnd = null;
+//  private bool $isServerSideJsLoading = true; // Future feature: Always load js and check in js if the logs should be shown
 
   public function setLog(string $log): phpJsConsoleLogger
   {
@@ -28,26 +31,42 @@ class phpJsConsoleLogger
     return $this;
   }
 
-  public function generateJs()
+  public function generateJs(): string
   {
+    // TODO Check if all required parameters are set
+
     $stringToReturn = "
       <script type=\"text/javascript\"> 
         let logs = " . (!empty($this->log) ? json_encode([$this->log]) : json_encode($this->logs)) . ";
+        let timeout = 1000;
         let interval = 2000;
-        let timeout = 5000;
+//        let maxIterations = 0;
         
         setTimeout(function() {
-          logs.forEach (function(val) {
-            setTimeout(function () {
-            
-              console.log(val);
-              
-            }, interval);
-          });
+          function processLog() {
+            if (logs.length > 0) {
+              setTimeout(function () {
+                console.log(logs[0]);
+                logs.shift();
+                processLog();
+              }, interval);
+            }
+          }
+          processLog();
         }, timeout);
       </script>
     ";
 
     return $stringToReturn;
+  }
+
+  private function validateDateTime()
+  {
+
+  }
+
+  private function validateRequiredParameters()
+  {
+
   }
 }
