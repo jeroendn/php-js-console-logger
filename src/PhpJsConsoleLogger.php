de5e334ef9a4578b7968285ddcb0995082a6732b
jeroendn <51778824+jeroendn@users.noreply.github.com>
2021-10-10 11:21:18 +0200

    Removed deprecated generateJs function

--- src/PhpJsConsoleLogger.php
+++ src/PhpJsConsoleLogger.php
@@ -11,60 +11,6 @@ class PhpJsConsoleLogger extends PhpJsConsoleLoggerBase
      * Generates the JS in an HTML script tag
      * @return string
      * HTML
-     * @deprecated Use getHtml() or getJs() instead
-     */
-    public function generateJs(): string
-    {
-        return "
-            <script type=\"text/javascript\">
-                let iterations = " . $this->iterations . ";
-                let iterationSpacer = '" . $this->iterationSpacer . "';
-                let logsOriginal = " . (!empty($this->log) ? json_encode([$this->log]) : json_encode($this->logs)) . ";
-                let logs = ('" . $this->iterationSpacer . "' !== '' && iterations !== 1) ? [].concat(logsOriginal, ['" . $this->iterationSpacer . "']) : [].concat(logsOriginal); // TODO perhaps write a function for this
-                let timeout = " . $this->timeout . ";
-                let interval = " . $this->interval . ";
-
-                // If iterations is 0, don't run the code at all
-                if (iterations !== 0) {
-
-                    // Timeout
-                    setTimeout(function() {
-
-                        // Log the given data
-                        function processLog() {
-                            if (logs.length > 0) {
-                                setTimeout(function () {
-                                    console.log(logs[0]);
-                                    logs.shift();
-                                    processLog();
-                                }, interval);
-
-                            // Prepare for a new iteration
-                            } else if(iterations !== 1) {
-                                setTimeout(function () {
-                                    logs = (iterationSpacer !== '' && iterations !== 2) // Add spacer when set and not last iteration
-                                            ? logs.concat(logsOriginal, [iterationSpacer])
-                                            : logs.concat(logsOriginal);
-                                    if(iterations > 0) iterations--;
-                                    processLog();
-                                }, timeout)
-                            }
-                        }
-
-                        // Initial start trigger
-                        processLog();
-
-                    }, timeout);
-
-                }
-            </script>
-        ";
-    }
-
-    /**
-     * Generates the JS in an HTML script tag
-     * @return string
-     * HTML
      */
     public function getHtml(): string
     {
